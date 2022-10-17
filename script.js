c65dfed6f61ad9d264eda2862333a2047105192d
강혁준 <gurwns232@naver.com>
2022-10-17 22:02:57 +0900

    searchBtn click event update

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const searchLine = document.querySelector('.search-data .line')
 searchBtn.addEventListener('click', () => {
     wrapper.classList.add('active')
     searchBtn.style.display = 'none'
+    console.log(wrapper)
+    console.log(searchLine)
 
     searchLine.classList.add('active')
 })
