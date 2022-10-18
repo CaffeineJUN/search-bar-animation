55c866267745b5c0a62536095b3e1c4eac94caa2
강혁준 <gurwns232@naver.com>
2022-10-18 22:19:42 +0900

    tag style update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const searchBtn = document.querySelector('.search-btn')
 const wrapper = document.querySelector('.wrapper')
 const searchLine = document.querySelector('.search-data .line')
+const searchData = document.querySelector('.search-data')
+const input = document.querySelector('.search-data input')
+const closeBtn = document.querySelector('.close-btn')
 
 searchBtn.addEventListener('click', () => {
     wrapper.classList.add('active')
+    console.log(searchData)
+    searchData.style.visibility = 'visible'
     searchBtn.style.display = 'none'
-    console.log(wrapper)
-    console.log(searchLine)
 
     searchLine.classList.add('active')
 })
